fea80ef535ced6781e16806b51abe5fa78403e90
kerszl <45152848+kerszl@users.noreply.github.com>
2021-07-17 03:20:43 +0200

    Update cookie-consent.js

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function(){
     },
    content: {
        link: "About consent",
-       href: "https://2gdpr.com/cookies"
+       href: "https://kerszl.github.io/terms/"
     },
     onInitialise: function(status) {
       if(status == cookieconsent.status.allow) myScripts();
@@ -35,20 +35,9 @@ function myScripts() {
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-00000000-1', 'auto');
+      ga('create', 'UA-199157698-1', 'auto');
       ga('send', 'pageview');
 
 
-   // Facebook Pixel Code, you need to change '000000000000000' to your PixelID
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '000000000000000');
-      fbq('track', 'PageView');
 
 }
