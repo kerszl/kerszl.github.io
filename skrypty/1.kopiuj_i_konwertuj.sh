acf738cca0479e58aba6a3ad7bfcf77c7814d1d1
szikers <kerszi@protonmail.com>
2023-09-26 16:45:34 +0200

    testy

--- skrypty/1.kopiuj_i_konwertuj.sh
+++ skrypty/1.kopiuj_i_konwertuj.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 YEAR=$(date +'%Y')
+
+#ANDROID
 PICTURES_BLOG_PATH='/data/data/com.termux/files/home/storage/pictures/blog'
-#PODROZ_PATH="/data/data/com.termux/files/home/repo/kerszl.github.io/assets/images/rower/$YEAR/$1"
-PODROZ_PATH="/mnt/d/moje programy i inne/GNU/kerszl.github.io/assets/images/rower/$YEAR/$1"
 
+#ANDROID
+PODROZ_PATH="/data/data/com.termux/files/home/repo/kerszl.github.io/assets/images/rower/$YEAR/$1"
+#PC
+#PODROZ_PATH="/mnt/d/moje programy i inne/GNU/kerszl.github.io/assets/images/rower/$YEAR/$1"
 
+#ANDROID
+SKRYPT_PATH='/data/data/com.termux/files/home/repo/kerszl.github.io/skrypty/'
+#PC
+#SKRYPT_PATH='/mnt/d/moje programy i inne/GNU/kerszl.github.io/skrypty/'
+
+TEKST_DO_WKLEJENIA=$SKRYPT_PATH"tekst_do_wklejenia.txt"
+SZABLON01="$SKRYPT_PATH"szablon01.txt
+SZABLON02="$SKRYPT_PATH"szablon02.txt
+
+#POST_PATH="/data/data/com.termux/files/home/repo/kerszl.github.io/_posts/"
+POST_PATH="/mnt/d/moje programy i inne/GNU/kerszl.github.io/_posts/"
+
+#1 etap - sprawdzanie katalogow
 
 if [ $# -eq 1 ]; then
 	if [ ! -d "$PODROZ_PATH" ]; then
@@ -33,28 +50,22 @@ exit
 fi
 
 
+if [ ! -d "$PODROZ_PATH/$2" ]; then
+mkdir "$PODROZ_PATH/$2"
+fi
+
+
 if [ -f "$PODROZ_PATH"/$2/01.jpg ]; 
 then
 	echo Uwaga: sa pliki z obrazkami, skasuj recznie
 	exit
 fi
 
-if [ ! -d "$PODROZ_PATH/$2" ]; then
-mkdir "$PODROZ_PATH/$2"
-fi
-
-SCIEZKA_WIELKIEJ_PODROZY="$PODROZ_PATH/$2"
 
 
-Y_M_D=$(date +'%Y-%m-%d-')
-PLIK_MD=$Y_M_D$1"-"$2".md"
-echo $PLIK_MD
 
-exit
 
-./2.edytuj_post.sh $SCIEZKA_WIELKIEJ_PODROZY
-exit
-
+#2 etap
 count=1
 
 echo zmienian nazwy na cyfry i kopiuje do $1
@@ -76,4 +87,54 @@ done
 
 echo kopiuje mapke  - mapka.png
 cp -f $PICTURES_BLOG_PATH/*.png "$PODROZ_PATH/$2"/mapka.png
+
+
+#---3 etap
+#przeniesc na koniec - poczatek
+
+SCIEZKA_WIELKIEJ_PODROZY="$PODROZ_PATH/$2/"
+
+if [ ! "$(ls -A "$SCIEZKA_WIELKIEJ_PODROZY")" ]; then
+    echo "Brak plikow w $SCIEZKA_WIELKIEJ_PODROZY"
+	exit
+fi	
+	
+
+Y_M_D=$(date +'%Y-%m-%d-')
+PLIK_MD=$Y_M_D$1"-"$2".md"
+POST_FILE_PATH="$POST_PATH$PLIK_MD"
+
+echo Tworze i edytuje plik: "$POST_FILE_PATH"
+
+
+echo --- > "$POST_FILE_PATH"
+echo -n "title: " >> "$POST_FILE_PATH"
+sed -n '1{s/.*/"&"/p}' "$TEKST_DO_WKLEJENIA" >> "$POST_FILE_PATH"
+cat "$SZABLON01" >> "$POST_FILE_PATH"
+
+OBRAZKI_JPG=("$SCIEZKA_WIELKIEJ_PODROZY"*.jpg)
+
+
+for plik in "${OBRAZKI_JPG[@]}"; do
+echo -n "  - url: " >> "$POST_FILE_PATH"
+echo ${plik/*kerszl.github.io/} >> "$POST_FILE_PATH"
+echo -n "    image_path: " >> "$POST_FILE_PATH"
+echo ${plik/*kerszl.github.io/} >> "$POST_FILE_PATH"
+done
+
+echo "---" >> "$POST_FILE_PATH"
+echo '<!---'>> "$POST_FILE_PATH"
+echo '<a href="https://connect.garmin.com/modern/activity/embed/wpisz_numer" onclick="window.open(this.href); return false;">' >> "$POST_FILE_PATH"
+echo "[![mapka]("${SCIEZKA_WIELKIEJ_PODROZY/*kerszl.github.io/}"mapka.png)]" >> "$POST_FILE_PATH"
+echo '</a>' >> "$POST_FILE_PATH"
+echo '-->'>> "$POST_FILE_PATH"
+
+echo "[![mapka]("${SCIEZKA_WIELKIEJ_PODROZY/*kerszl.github.io/}"mapka.png)]" >> "$POST_FILE_PATH"
+echo >> "$POST_FILE_PATH"
+sed '1d;2,${/[[:alpha:]]/,$!d};s/^$/{: .text-justify}/'  "$TEKST_DO_WKLEJENIA" >> "$POST_FILE_PATH"
+cat "$SZABLON02" >> "$POST_FILE_PATH"
+
+
+sed -i "s/NAZWA_SPECJALNA_WYCIECZKI/$1/" "$POST_FILE_PATH"
+
 
--- skrypty/generuj.sh
+++
@@ -1,9 +0,0 @@
-#plik był w katalogu _posts
-dzien=1
-for i in {03..31}
-do
-sed '2s/Dzień:/Dzień: '$dzien'/; 16,$s/wielka_podroz\/01/wielka_podroz\/'$i/ 2023-08-wielka-podroz-szablon.md > 2023-08-$i-wielka-podroz.md
-((dzien++))
-done
-
-echo android
