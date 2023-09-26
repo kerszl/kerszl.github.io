696bcccd618832f3dfbcffb0d0358c6963aa98e3
szikers <kerszi@protonmail.com>
2023-09-26 18:33:21 +0200

    bug

--- skrypty/2.git.sh
+++ skrypty/2.git.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+REPO_PATH='data/data/com.termux/files/home/repo/kerszl.github.io/'
 if [ $# -le 0 ]; then
 echo parametr to wpis do gita
 exit
 fi
 
-cd kerszl.github.io
+cd $REPO_PATH
 git add .         
 git commit -m $1
 git push
